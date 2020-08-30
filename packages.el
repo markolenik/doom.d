c37961292eb6c839462dcf8a67b4d6125d0aacdd
Mark Olenik <mark.olenik@gmail.com>
2020-08-30 22:54:57 +0200

    Unpin helm-bibtex to use latest dev version

--- packages.el
+++ packages.el
@@ -17,4 +17,7 @@
 (package! evil-nerd-commenter :disable t)
 (package! evil-commentary)
 
-;; Jump between sentences in text
+;; `helm-bibtex' is pinned in `modules/tools/biblio/packages.el'
+;; Use latest version of `helm-bibtex' instead.
+(when (featurep! :completion helm)
+  (package! helm-bibtex :pin nil))
