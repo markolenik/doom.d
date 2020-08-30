f57ab5bc06a09fc71a8e445904e168a71830b80f
Mark Olenik <mark.olenik@gmail.com>
2020-08-30 22:58:53 +0200

    Use flyspell instead of spell-ful
    Reason: flyspell is a bit slower but adds a lot of convenience,
    especially the helm integration is much better.

--- init.el
+++ init.el
@@ -90,7 +90,9 @@
 
        :checkers
        ;; syntax              ; tasing you for every semicolon you forget
-       spell             ; tasing you for misspelling mispelling
+       (spell +flyspell
+              ;; +spell-fu
+              +aspell)             ; tasing you for misspelling mispelling
        ;; grammar           ; tasing grammar mistake every you make
 
        :tools
