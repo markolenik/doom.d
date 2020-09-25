bf487c3483d1385980e2d7fa6fff30af79d7eece
Mark Olenik <mark.olenik@gmail.com>
2020-09-25 21:52:28 +0200

    Restructure packages a bit

--- packages.el
+++ packages.el
@@ -2,22 +2,17 @@
 ;;; $DOOMDIR/packages.el
 
 (unpin! bibtex-completion org-roam)
+(disable-packages! company-org-roam evil-nerd-commenter)
 
 ;; Change frame splits
 (package! transpose-frame)
 
-;; At the moment I like `evil-commentary' more than
-;; `evil-nerd-commenter'. Might change in future if I understand how
-;; to replicate all features.
-(package! evil-nerd-commenter :disable t)
 (package! evil-commentary)
 (package! visual-fill-column)
 (package! jupyter)
 (package! org-ref)
 (package! org-roam-bibtex)
 (package! org-roam-server)
-;; company-org-roam makes writing text very slow
-(package! company-org-roam :disable t)
 ;; Toggle org-mode latex fragment on cursor hover
 (package! org-fragtog)
 
