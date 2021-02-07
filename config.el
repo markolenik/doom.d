4e542faaf89d67bc7f04ac2dbdf5a3e12b0a038e
Mark Olenik <mark.olenik@gmail.com>
2021-02-07 17:24:47 +0100

    Change org heading height
    See stackoverflow.com/questions/21525436/orgmode-title-levels-height

--- config.el
+++ config.el
@@ -234,6 +234,14 @@
                                 :background nil t))))
   (advice-add #'load-theme :after #'+org-update-latex-preview-background-color)
 
+  ;; Make org headings be normal size
+  (custom-set-faces
+   '(org-level-1 ((t (:inherit outline-1 :height 1.0))))
+   '(org-level-2 ((t (:inherit outline-2 :height 1.0))))
+   '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
+   '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
+   '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
+   )
   )
 
 
